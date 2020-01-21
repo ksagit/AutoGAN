ff151df25943284c4c54fe26e14aad0d468a033d
KyleSargent <kylesargent8@gmail.com>
2020-01-21 11:20:45 -0500

    Add tests and autograd func

--- models/neighbor_discriminator.py
+++ models/neighbor_discriminator.py
@@ -3,6 +3,33 @@ import torch.nn as nn
 import torch
 import numpy as np
 import faiss
+from torch.autograd import Function
+
+
+class NeighborDiscriminatorFunction(Function):
+
+    @staticmethod
+    def forward(ctx, input, model):
+        D, I = model.get_approximated_neighbor_activations(input)
+        D_actual, I_actual = model.get_actual_distances(D, I)
+        D, I = model.get_maximal_neighbor_activations(D_actual, I_actual)
+        neighbors = model.X[I]
+
+        D_actual_at_maxes_mask = torch.cat(
+            [
+                (I_actual[:, col] == I).unsqueeze(1)
+                for col in range(I_actual.shape[1])
+            ], dim=1
+        )
+        D_actual_at_maxes = D_actual.masked_select(D_actual_at_maxes_mask)
+        scaled_stabilized_D_actual_at_maxes = D_actual_at_maxes / model.K + 1e-5
+        ctx.save_for_backward(input, neighbors, scaled_stabilized_D_actual_at_maxes)
+        return D, I
+
+    @staticmethod
+    def backward(ctx, grad_output, _grad_indices):
+        input, neighbors, scaled_D_actual = ctx.saved_tensors
+        return (neighbors - input) / scaled_D_actual.unsqueeze(1), None
 
 
 class NeighborDiscriminator(nn.Module):
@@ -12,13 +39,15 @@ class NeighborDiscriminator(nn.Module):
             X: torch.Tensor,
             K: float = 1,
             nlist: int = 100,
-            nprobe: int = 100,
+            nprobe: int = 10,
             k: int = 10,  # number of neighbors to check
+            steps: int = 1,
+            eta: float = .1
 
     ):
         super(NeighborDiscriminator, self).__init__()
         self.X = X.view(X.shape[0], -1)
-        self.w = nn.Parameter(torch.randn(X.shape[0], 1) * 10)
+        self.w = nn.Parameter(torch.zeros(X.shape[0], 1))
         self.K = K
 
         self.n, self.d = self.X.shape
@@ -26,68 +55,89 @@ class NeighborDiscriminator(nn.Module):
         self.nprobe = nprobe
         self.k = k
 
-    def w_prime(self):
+        self.eta = eta
+
+        # also initializes self.X_w, self.w_prime
+        self.update_index()
+
+    def get_w_prime(self):
         w_prime = -(self.w - torch.max(F.relu(self.w)))
         w_prime = torch.sqrt(w_prime / self.K)
         return w_prime
 
-    def X_w(self):
+    def get_X_w(self):
         X = self.X.data.cpu().numpy()
-        w_prime = self.w_prime().data.cpu().numpy()
-
+        w_prime = self.w_prime.data.cpu().numpy()
         return np.hstack([X, w_prime]).astype('float32')
 
-    def get_index(self):
+    def update_index(self):
+        self.w_prime = self.get_w_prime()
+        self.X_w = self.get_X_w()
         index = faiss.IndexFlatL2(self.d + 1)
-        index.add(self.X_w())
-        return index
+        index.add(self.X_w)
+        self.index = index
 
-    def get_approximated_neighbors(self, X_tilde):
+    def get_approximated_neighbor_activations(self, X_tilde):
+        """Get the nearest neighbors in \|x_i \oplus \sqrt{w_i'/K} - x \oplus 0\| """
         X_tilde = X_tilde.view(X_tilde.shape[0], -1)
-        self.X_tilde_padded = np.hstack(
+        X_tilde_padded = np.hstack(
             [
                 X_tilde.data.cpu().numpy(),
                 np.zeros((X_tilde.shape[0], 1))
             ]
         ).astype('float32')
 
-        index = self.get_index()
+        index = self.index
 
-        D, I = index.search(self.X_tilde_padded, self.k)
+        D, I = index.search(X_tilde_padded, self.k)
         return D, I
 
     def get_actual_distances(self, D, I):
+        """Adjust the approximated neighbor activations to get the \|x_i - x\|"""
         D, I = torch.from_numpy(D), torch.from_numpy(I)
 
-        local_w_adjustments = self.w_prime()[I].squeeze(2) ** 2
-        D_actual_squared = F.relu(D - local_w_adjustments)
+        local_w_adjustments = self.w_prime[I].squeeze(2)
+        D_actual_squared = F.relu(D - local_w_adjustments ** 2)
 
         D_actual = torch.sqrt(D_actual_squared)
         return D_actual, I
 
-    def get_maximal_neighbors(self, D_actual, I):
+    def get_maximal_neighbor_activations(self, D_actual, I):
+        """Use the \|x_i - x\| to get the w_i - K \|x_i - x\|, and find the max and argmax over i"""
         f_i_values = self.w[I].squeeze(2) - self.K * D_actual
         minimal_distance_indices = torch.argmax(f_i_values, axis=1)
-        return torch.Tensor([row[index] for row, index in zip(I, minimal_distance_indices)])
+        D_I = torch.Tensor(
+            [
+                (dist_row[index], index_row[index])
+                for dist_row, index_row, index in zip(f_i_values, I, minimal_distance_indices)
+            ]
+        )
+        return D_I[:, 0], D_I[:, 1].long()
 
     def forward(self, X_tilde):
-        D, I = self.get_approximated_neighbors(X_tilde)
-        D, I = self.get_actual_distances(D, I)
-        return dis.get_maximal_neighbors(D, I)
+        return NeighborDiscriminatorFunction.apply(X_tilde, self)
+
+    def train(self, X_tilde):
+        with torch.no_grad():
+            _, I = self(X_tilde)
+            counts = torch.zeros(self.n)
+            for label in I:
+                counts[label] += 1
+
+            update_indices = torch.where(counts > 0)
+            self.w -= counts.unsqueeze(1) * self.eta
+            self.w.data[update_indices] = self.forward(self.X[update_indices])[0].unsqueeze(1)
+            self.w -= self.w.mean()
 
 
 class QuantizedNeighborDiscriminator(NeighborDiscriminator):
 
-    def __init__(self, *args, **kwargs):
-        super(QuantizedNeighborDiscriminator, self).__init__(*args, **kwargs)
+    def update_index(self):
+        self.w_prime = self.get_w_prime()
+        self.X_w = self.get_X_w()
 
-    def get_index(self):
-        quantizer = faiss.IndexFlatL2(self.d + 1)  # the other index
+        quantizer = faiss.IndexFlatL2(self.d + 1)
         index = faiss.IndexIVFFlat(quantizer, self.d + 1, self.nlist)
-
-        xb = self.X_w()
-        assert not index.is_trained
-        index.train(xb)
-        assert index.is_trained
-        index.add(xb)
-        return index
+        index.train(self.X_w)
+        index.add(self.X_w)
+        self.index = index
---
+++ models/test_neighbor_discriminator.py
@@ -0,0 +1,63 @@
+import unittest
+from models.neighbor_discriminator import NeighborDiscriminator
+import torch
+from torch.autograd import grad
+
+
+def torch_pairwise_distances(X, Y):
+    return torch.sqrt(torch.abs(
+        torch.sum(X * X, dim=1, keepdim=True)
+        - 2 * torch.matmul(X, Y.transpose(0,1))
+        + torch.sum(Y * Y, dim=1, keepdim=True).transpose(0,1)
+    ))
+
+
+class TestNeighborDiscriminator(unittest.TestCase):
+
+    def setUp(self):
+        self.X = torch.randn(500, 3 * 32 * 32)
+        self.x_gen = torch.randn(2, 3 * 32 * 32)
+        self.x_gen.requires_grad = True
+        self.dis = NeighborDiscriminator(self.X, k=5, K=10)
+        self.dis.w.data = torch.randn_like(self.dis.w.data)
+        self.dis.update_index()
+
+
+    def test_neighbor_activation_correctness(self):
+        """For the baseline (exact) NeighborDiscriminator, we should have"""
+        maximal_neighbor_activations = self.dis(self.x_gen)[0]
+
+        pairwise_distances = torch_pairwise_distances(self.X, self.x_gen)
+        exact_neighbor_activations = -self.dis.K * pairwise_distances + self.dis.w.data
+        exact_maximal_neighbor_activations, indices = torch.max(exact_neighbor_activations, axis=0)
+
+        self.assertEqual(maximal_neighbor_activations.shape, exact_maximal_neighbor_activations.shape)
+        errs = torch.abs(maximal_neighbor_activations - exact_maximal_neighbor_activations)
+
+        # This can be a little bigger because torch pairwise distance calcs are notoriously unstable
+        self.assertTrue(all(errs < 1e-3))
+
+    def test_input_gradient_correctness(self):
+        """The gradient for each \tilde x_j \in x_gen is of the form
+
+            -K (\tilde x_j - x_i) / (\|x_j - x_i)
+
+            Where i is the index of the maximal neighbor activation.
+        """
+        # Get the gradient from the model
+        d_gen = self.dis(self.x_gen)[0]
+        loss_gen = torch.sum(d_gen)
+        grad_model = grad(outputs=loss_gen, inputs=self.x_gen)
+
+        # Get the exact gradient of the maximal neighbor activation w.r.t the input
+        pairwise_distances = torch_pairwise_distances(self.X, self.x_gen)
+        exact_neighbor_activations = -self.dis.K * pairwise_distances + self.dis.w.data
+        exact_maximal_neighbor_activations, _ = torch.max(exact_neighbor_activations, axis=0)
+        loss_gen_exact = torch.sum(exact_maximal_neighbor_activations)
+        grad_exact = grad(outputs=loss_gen_exact, inputs=self.x_gen)
+
+        errs = torch.abs(grad_exact[0] - grad_model[0]).view(-1)
+        self.assertTrue(all(errs < 1e-5))
+
+
+
